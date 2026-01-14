efe95347337d9b052c001585d25a7601f7d6b669
mohamed abdelateef <mohamedabdelateef25@gmail.com>
2026-01-14 10:49:20 +0200

    update mode paths

---
+++ app/Models/Tenant/VtigerSostatus.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Tenant;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VtigerSostatus extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'id' => 'integer',
+        ];
+    }
+}
