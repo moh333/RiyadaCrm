fce30fd8c2f47f60300d3aab510252105dd12024
mohamed abdelateef <mohamedabdelateef25@gmail.com>
2026-02-02 17:57:37 +0200

    Set default current timestamps for `createdtime` and `modifiedtime` in `vtiger_crmentity` table.

---
+++ database/migrations/tenant/2026_02_02_154826_update_vtiger_crmentity_timestamps_default.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vtiger_crmentity', function (Blueprint $table) {
+            $table->dateTime('createdtime')->nullable()->useCurrent()->change();
+            $table->dateTime('modifiedtime')->nullable()->useCurrent()->useCurrentOnUpdate()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('vtiger_crmentity', function (Blueprint $table) {
+            $table->dateTime('createdtime')->default(null)->nullable()->change();
+            $table->dateTime('modifiedtime')->default(null)->nullable()->change();
+        });
+    }
+};
